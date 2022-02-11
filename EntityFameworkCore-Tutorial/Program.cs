cdd35752a7512f84fecd399968cc9377d6e2a5dd
hayden brown <haydenbrowncinci@gmail.com>
2022-02-11 09:34:20 -0500

    Add project files.

---
+++ EntityFameworkCore-Tutorial/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+//Using CodeFirst allows you to work in C# while Entity Packages manages the SQL aspect
+namespace EntityFameworkCore_Tutorial {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
