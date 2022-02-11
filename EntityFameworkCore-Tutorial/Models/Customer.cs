2b169c5e848630cbd58c12e134f758f8b577941f
hayden brown <haydenbrowncinci@gmail.com>
2022-02-11 09:41:53 -0500

    added customer class in model

--- EntityFameworkCore-Tutorial/Models/Customer.cs
+++ EntityFameworkCore-Tutorial/Models/Customer.cs
@@ -5,6 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace EntityFameworkCore_Tutorial.Models {
-    class Customer {
+    public class Customer {
+        public int Id { get; set; }                 // EntitiyFramework(EF) will assume 'Id' is the primary key
+        public string Name { get; set; }
+        public bool Active { get; set; } //= true;
+        public decimal Sales { get; set; }
+        public DateTime Updated { get; set; }
     }
 }
