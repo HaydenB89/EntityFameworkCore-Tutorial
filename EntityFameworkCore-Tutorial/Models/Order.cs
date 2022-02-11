c831c28d8a3e7a69634084d4566afa4f7a640548
hayden brown <haydenbrowncinci@gmail.com>
2022-02-11 13:42:09 -0500

    added order model

---
+++ EntityFameworkCore-Tutorial/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFameworkCore_Tutorial.Models {
+    public class Order {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Total { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
